02957e169489b9ce0a83b55a095ba6d7da5b9f29
Денис Смирнов <deadpoolair@gmail.com>
2024-06-13 15:43:02 +0700

    first blood

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,43 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import datetime as dt
+import csv
+import os
 
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+from .settings import (
+    BASE_DIR,
+    RES_DIR,
+    RES_DIR_NAME,
+    DT_FORMAT,
+    HEAD_PEP_STATUS,
+    FOOT_PEP_STATUS
+    )
 
 
 class PepParsePipeline:
+
+    def __init__(self):
+        RES_DIR.mkdir(exist_ok=True)
+        self.statuses = {}
+
+    def open_spider(self, spider):
+        pass
+
     def process_item(self, item, spider):
+        self.statuses[item['status']] = self.statuses.get(
+            item['status'], 0) + 1
         return item
+
+    def close_spider(self, spider):
+        now_time = dt.datetime.now().strftime(DT_FORMAT)
+        file_name = f'{RES_DIR_NAME}/status_summary_{now_time}.csv'
+        file_path = os.path.join(BASE_DIR, file_name)
+        with open(file_path, 'w', encoding='utf-8') as f:
+            writer = csv.writer(
+                f, dialect=csv.unix_dialect, quoting=csv.QUOTE_MINIMAL
+            )
+            writer.writerows(
+                [
+                    HEAD_PEP_STATUS,
+                    *self.statuses.items(),
+                    (FOOT_PEP_STATUS, sum(self.statuses.values())),
+                ]
+            )
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,10 +1,27 @@
+import re
+
 import scrapy
+
+from pep_parse.items import PepParseItem
 
 
 class PepSpider(scrapy.Spider):
+
     name = 'pep'
     allowed_domains = ['peps.python.org']
     start_urls = ['http://peps.python.org/']
 
     def parse(self, response):
-        pass
+        for pep_link in response.css('a[href^="pep"]'):
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        pattern = r'PEP (?P<version>\d+) – (?P<name>.*)'
+        pep_number, pep_title = re.search(
+            pattern, response.css('#pep-content h1::text').get()
+        ).groups()
+        yield PepParseItem(
+            number=pep_number,
+            name=pep_title,
+            status=response.css('dt:contains("Status") + dd ::text').get()
+        )
